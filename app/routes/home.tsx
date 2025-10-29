dc77a5bbf07fac2275a802969a7b98e7a7fcc333
Marina Majdenić <marina.majden@gmail.com>
2025-10-29 02:27:15 +0100

    refactor: improve JSX structure in Home component for better readability

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,5 +2,9 @@ import InteractiveTimeline from "../components/InteractiveTimeline";
 import { artTimelineData } from "../data/artTimelineData";
 
 export default function Home() {
-    return <InteractiveTimeline data={artTimelineData} />;
+    return (
+        <>
+            <InteractiveTimeline data={artTimelineData} />
+        </>
+    );
 }
