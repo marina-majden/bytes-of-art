1cbb08025898f2f94d49c4886b26e54707fa4cc7
Marina Majdenić <marina.majden@gmail.com>
2025-11-14 02:12:44 +0100

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,8 @@
-import MagicBento from "~/components/BentoGrid";
+import MagicBento from "../components/bento/index";
 
 export default function HomePage() {
     return (
-        <div className='max-w-screen h-screen p-0 mx-auto overflow-hidden'>
+        <div className='w-screen h-screen p-0 mx-auto overflow-hidden'>
             <MagicBento />
         </div>
     );
